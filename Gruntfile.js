36f02c5cc9ff45a623839f75548e0205bd563203
Michał Budzyński <michal@virtualdesign.pl>
2014-12-20 01:01:00 +0100

    :exclamation: proper argument sent to the watcher

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,17 @@
 
 module.exports = function(grunt) {
 
+  var model = grunt.option('model') ? '?model=' + grunt.option('model') : '';
+
+  console.log(model);
+
   grunt.initConfig({
     browserSync: {
       bsFiles: {
         src : 'models/**/*.jscad'
       },
       options: {
+        startPath: model,
         server: {
           baseDir: './'
         }
