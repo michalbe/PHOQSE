f12def9448a594dbbfd287783ab5318c7a9c6f0b
Michał Budzyński <michal@virtualdesign.pl>
2014-12-20 01:01:00 +0100

    :checkered_flag: refactor openjscad file

--- js/openjscad-index.js
+++ js/openjscad-index.js
@@ -1,58 +1,49 @@
 var gProcessor = null;
 var editor = null;
 var _includePath = './';
-
+var key = 'Shift-Return';
 var version = '0.018 (2014/10/05)';
-//$(function() { $("#parametersdiv").draggable(); }); // doesn't work well, disabled
-var me = document.location.toString().match(/^file:/)?'web-offline':'web-online'; // me: {cli, web-offline, web-online}
+var me = 'web-online'; // me: {cli, web-offline, web-online}
 var browser = 'unknown';
-if(navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)/i))
+
+if (navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)/i)) {
   browser = RegExp.$1.toLowerCase();
+}
 
-  $(document).ready(function() {
-    $("#viewer").height($(window).height());
+$(document).ready(function() {
+  $('#viewer').height($(window).height());
 
-    $(window).resize(function() {                // adjust the relevant divs
-      $("#viewer").width($(window).width());
-      $("#viewer").height($(window).height());
-    });
+  $(window).resize(function() { // adjust the relevant divs
+    $('#viewer').width($(window).width());
+    $('#viewer').height($(window).height());
+  });
+});
+
+var exec = function(editor) {
+  var src = editor.getValue();
+  if (src.match(/^\/\/\!OpenSCAD/i)) {
+    editor.getSession().setMode('ace/mode/scad');
+    src = openscadOpenJscadParser.parse(src);
+  } else {
+    editor.getSession().setMode('ace/mode/javascript');
+  }
+  gMemFs = [];
+  gProcessor.setJsCad(src);
+};
+
+window.onload = function() {
+  editor = ace.edit('editor');
+  editor.setTheme('ace/theme/chrome');
+  editor.getSession().setMode('ace/mode/javascript');
+  editor.commands.addCommand({
+    name: 'myCommand',
+    bindKey: {
+      win: key,
+      mac: key
+    },
+    exec: exec
   });
 
-  function onload() {
-    editor = ace.edit("editor");
-    editor.setTheme("ace/theme/chrome");
-    editor.getSession().setMode("ace/mode/javascript");
-    editor.getSession().on('change', function(e) {
-      ;
-    });
-    ['Shift-Return'].forEach(function(key) {
-      editor.commands.addCommand({
-        name: 'myCommand',
-        bindKey: { win: key, mac: key },
-        exec: function(editor) {
-          var src = editor.getValue();
-          if(src.match(/^\/\/\!OpenSCAD/i)) {
-            editor.getSession().setMode("ace/mode/scad");
-            src = openscadOpenJscadParser.parse(src);
-          } else {
-            editor.getSession().setMode("ace/mode/javascript");
-          }
-          gMemFs = [];
-          gProcessor.setJsCad(src);
-        },
-      });
-    });
-
-    gProcessor = new OpenJsCad.Processor(document.getElementById("viewer"));
-    (function(editor) {
-      var src = editor.getValue();
-      if(src.match(/^\/\/\!OpenSCAD/i)) {
-        editor.getSession().setMode("ace/mode/scad");
-        src = openscadOpenJscadParser.parse(src);
-      } else {
-        editor.getSession().setMode("ace/mode/javascript");
-      }
-      gMemFs = [];
-      gProcessor.setJsCad(src);
-    })(editor);
-  }
+  gProcessor = new OpenJsCad.Processor($('#viewer')[0]);
+  exec(editor);
+};
