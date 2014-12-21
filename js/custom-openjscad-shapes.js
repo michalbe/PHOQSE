b8d66730acaf42a7adb492762d6252892710a3d3
Michał Budzyński <michal@virtualdesign.pl>
2014-12-21 01:01:00 +0100

    :mount_fuji: new globals in customShapes

--- js/custom-openjscad-shapes.js
+++ js/custom-openjscad-shapes.js
@@ -1,6 +1,38 @@
 var customShapesDefinitions = function() {
+
+  /**
+  *
+  * Globals
+  *
+  */
+  // size of the hole for the screw
   var screwHoleSize = 0.7;
-  var pinHoleSize = 0.5;
+
+  // size of the pinhole
+  var pinHoleSize = 0.3;
+
+  // alphabet wheel's hole size
+  var alphabetHoleSize = 7;
+
+  // cylinder ring's wall thickness
+  // front face ring needs to fit inside, and both need to fit
+  // in the alphabet wheel (element 3)
+  var cylinderThickness = 0.5;
+
+  // hole needed to put element nr 8
+  var hubRadius = 0.75;
+  var hubDiameter = hubRadius * 2;
+
+  // radius for positioning screws in elements 2 & 10
+  var fontScrewRadius = 3;
+
+  // radius for positioning pins in elements 2 & 10 (and 11)
+  var pinRadius = 6;
+ /**
+  *
+  * Shapes
+  *
+  **/
   var hole = function(base, size, translate){
     return difference(base, cylinder({
       r: size,
